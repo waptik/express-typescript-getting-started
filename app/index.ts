8175e4dbfa5b810ba66bbc45a572ab95f095484c
Cristi Miloiu <miloiuc4@gmail.com>
2024-11-15 22:34:08 +0200

    Remove serverless adapter

--- app/index.ts
+++ app/index.ts
@@ -1,21 +1,19 @@
-import express, {Express} from "express";
-import serverless from "serverless-http";
+import express, { Express } from "express";
 
 const app: Express = express();
-const port = 3000;
+const port = 8080;
 
 app.get("/", (_req, res) => {
   res.send("Hello World from Express serverless!");
 });
 
 app.get("/users", (_req, res) => {
-  res.json([{id: 1, name: "Alice"}, {id: 2, name: "Bob"}]);
+  res.json([
+    { id: 1, name: "Alice" },
+    { id: 2, name: "Bob" },
+  ]);
 });
 
-if (process.env.NODE_ENV === "dev") {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-}
-
-export const handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
